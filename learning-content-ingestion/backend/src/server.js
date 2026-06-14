d9db4f8d02e0805d2cd64bee8519d5e12e239d07
sakthi-ckumar <sakthisivachandrakumar@gmail.com>
2026-06-14 15:01:46 +0530

    vercel fix

--- learning-content-ingestion/backend/src/server.js
+++ learning-content-ingestion/backend/src/server.js
@@ -5,10 +5,14 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 
-dotenv.config({ path: path.join(__dirname, '../.env') });
+dotenv.config();
 
 const app = express();
-app.use(cors("*"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json({ limit: '10mb' }));
 
 app.use('/api/content', require('./routes/contentRoutes'));
@@ -17,14 +21,14 @@ app.get('/', (req, res) => {
   res.send('Learning Content Ingestion API is running');
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 500
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected successfully');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(error => {
     console.error('MongoDB connection failed:', error.message);
-    process.exit(1);
   });
+
+module.exports = app;
